e8cc16a24b2cab841f66d89444b4d5fd5ea6d366
KoteykaTheCat <69836663+KoteykaTheCat@users.noreply.github.com>
2025-07-29 00:01:47 +0500

    Update ktc_dreams.js

--- js/ktc_dreams.js
+++ js/ktc_dreams.js
@@ -1,42 +1,40 @@
-const _dreams_list = document.getElementById('dreams');
-let _loaded_count = 0;
-const _batch_size = 5;
-let _dreams_i = [];
+window.addEventListener("DOMContentLoaded", () => {
+	const _dreams_list = document.getElementById('dreams');
+	let _loaded_count = 0;
+	const _batch_size = 5;
+	let _dreams_i = [];
 
-window.addEventListener('DOMContentLoaded', init);
-
-async function init() {
 	const _res = await fetch('dreams/index.json');
 	_dreams_i = await _res.json();
 
 	loadMoreDreams();
 
 	window.addEventListener('scroll', onScroll);
-}
 
-function onScroll() {
-	if (window.innerHeight + window.scrollY >= document.body.offsetHeight - 200) {
-		loadMoreDreams();
+	function onScroll() {
+		if (window.innerHeight + window.scrollY >= document.body.offsetHeight - 200) {
+			loadMoreDreams();
+		}
 	}
-}
 
-async function loadMoreDreams() {
-	if (_loaded_count >= _dreams_i.length) return;
+	async function loadMoreDreams() {
+		if (_loaded_count >= _dreams_i.length) return;
 
-	const _next_batch = _dreams_i.slice(_loaded_count, _loaded_count + _batch_size);
-	for (const i of _next_batch) {
-		const _res = await fetch(`dreams/${i}.json`);
-		if (!_res.ok) continue;
+		const _next_batch = _dreams_i.slice(_loaded_count, _loaded_count + _batch_size);
+		for (const i of _next_batch) {
+			const _res = await fetch(`dreams/${i}.json`);
+			if (!_res.ok) continue;
 
-		const _dream = await _res.json();
-		const _card = document.createElement('div');
-		_card.innerHTML = `
+			const _dream = await _res.json();
+			const _card = document.createElement('div');
+			_card.innerHTML = `
 			<a class="dream_p" href="dream?id=${encodeURIComponent(i)}">
 				<div class="title"> ${_dream.title} </div>
 				<div class="date"> ${_dream.date} </div>
 			</a>
 		`;
-		_dreams_list.appendChild(_card);
+			_dreams_list.appendChild(_card);
+		}
+		_loaded_count += _next_batch.length;
 	}
-	_loaded_count += _next_batch.length;
-}
+});
