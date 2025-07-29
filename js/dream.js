6d29e7855d8589f171a660bc001147f23cc879c2
KoteykaTheCat <69836663+KoteykaTheCat@users.noreply.github.com>
2025-07-29 16:24:35 +0500

    Update dream.js

--- js/dream.js
+++ js/dream.js
@@ -10,11 +10,13 @@ async function loadDream() {
 
 	try {
 		const _res = await fetch(`../dreams/${decodeURIComponent(_id)}.json`);
-		if (!_res.ok) throw new Error('Не найдено');
+		if (!_res.ok) {
+			throw new Error('Не найдено');
+		}
 		const _dream = await _res.json();
 		document.getElementById('title').textContent = _dream.title;
 		document.getElementById('date').textContent = _dream.date;
-		document.getElementById('text').textContent = _dream.text;
+		document.getElementById('text').innerHTML = _dream.text;
 		document.title = _dream.title;
 		document.getElementById('dream').classList.remove("hidden");
 	} catch (e) {
