e984f4cde72c0abca2ac87d7758be3b4cf2f02f0
KoteykaTheCat <69836663+KoteykaTheCat@users.noreply.github.com>
2025-08-21 22:38:53 +0500

    Delete js/gallery_src.js

--- js/gallery_src.js
+++
@@ -1,7 +0,0 @@
-function open_gallery(img) {
-	document.getElementById("gallery").classList.add("opened");
-	document.getElementById("gallery_img").src = img;
-};
-function close_gallery() {
-	document.getElementById("gallery").classList.remove("opened");
-};
